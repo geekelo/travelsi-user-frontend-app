11165046ba89c87fab456d9db50ccd0fd6efd746
Chandan-devs-tech <chandanguptabsb@gmail.com>
2024-05-25 01:14:15 +0530

    Installed some dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-orange': '#ff7022',
+      },
+    },
   },
   plugins: [],
 };
