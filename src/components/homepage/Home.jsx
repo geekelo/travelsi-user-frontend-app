6efd9b21a63f96c0be10f4bbf44f5ed6a3ed9d86
geekelo <efueliht12@gmail.com>
2024-05-28 19:48:26 +0100

    Add fies for homepage and landing sections

---
+++ src/components/homepage/Home.jsx
@@ -0,0 +1,11 @@
+import Landing from './Landing';
+
+function Home() {
+  return (
+    <div>
+      <Landing />
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/homepage/Landing.jsx
@@ -0,0 +1,12 @@
+function Landing() {
+  return (
+    <div>
+      <h1>Welcome to the Landing Page</h1>
+      <p>This is the landing page of our app.</p>
+      <h1>Welcome to the Landing Page</h1>
+      <p>This is the landing page of our app.</p>
+    </div>
+  );
+}
+
+export default Landing;
